aa612f34a810b664fa6add136967f4a3a0af1f65
Bina Co <binaco1400@gmail.com>
2021-08-09 16:40:35 +0430

    Add vote view

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 
 def index(request):
@@ -22,4 +23,17 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse(f"You are voting on question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': 'You did not select a choice.'
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
