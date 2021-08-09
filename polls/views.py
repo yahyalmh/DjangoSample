9a3435cec6b9029ff137a52e50d7da943b5555bc
Bina Co <binaco1400@gmail.com>
2021-08-09 13:55:49 +0430

    Add other views and their paths

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello World, you are at polls index")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"You are looking at result of {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You are voting on question {question_id}")
